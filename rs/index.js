5c561f614590c9048da819ce540541cff339d234
hana-rs <hanahanabi87@icloud.com>
2025-04-15 19:18:36 +0900

    index.js出力

--- rs/index.js
+++ rs/index.js
@@ -1,12 +1,1 @@
-const express = require('express');
-
-const app = express();
-const port = 8000;
-
-app.get('/', (req, res) => {
-    res.send('Hello, Hono.js!');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+console.log('Hello World !');
