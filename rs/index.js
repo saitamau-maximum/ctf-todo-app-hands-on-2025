12ed1a425d55987e5cbcccd914a7d87babe01823
hana-rs <hanahanabi87@icloud.com>
2025-04-15 19:16:56 +0900

    tesst

--- rs/index.js
+++ rs/index.js
@@ -1,1 +1,12 @@
-console.log('Hello World !');
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Hono.js!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
