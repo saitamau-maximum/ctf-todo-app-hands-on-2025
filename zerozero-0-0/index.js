ca4b157741cf895867d3bf37edbdcb7b8fc09702
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2025-04-16 22:49:39 +0900

    w to W

--- zerozero-0-0/index.js
+++ zerozero-0-0/index.js
@@ -5,7 +5,7 @@ const app = new Hono();
 
 const todos = [
     { id: 1, title: "JavaScriptを勉強する", completed: false },
-    { id: 2, title: "CTFのwriteupを書く", completed: false },
+    { id: 2, title: "CTFのWriteupを書く", completed: false },
     { id: 3, title: "学校の課題を提出する", completed: true },
 ];
 
