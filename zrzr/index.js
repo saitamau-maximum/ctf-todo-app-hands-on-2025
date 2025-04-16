7ea006d102f423b9c695bb2911a01ba264e45101
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2025-04-16 22:35:07 +0900

    add level2

--- zrzr/index.js
+++ zrzr/index.js
@@ -1,1 +1,19 @@
-console.log("Hello, Node.js!");
+const { Hono } = require("hono");
+const { serve } = require("@hono/node-server");
+
+const app = new Hono();
+
+const todos = [
+    { id: 1, title: "JavaScriptを勉強する", completed: false },
+    { id: 2, title: "CTFのwriteupを書く", completed: false },
+    { id: 3, title: "学校の課題を提出する", completed: true },
+];
+
+app.get("/", (c) => {
+    return c.json(todos, 200);
+})
+
+serve({
+    fetch: app.fetch,
+    port: 8000,
+});
