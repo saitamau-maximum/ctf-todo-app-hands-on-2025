08f4994583770159e2e7ed1363589fa1fbeed68a
nomanoma121 <takeuchi180121@gmail.com>
2025-04-23 15:32:25 +0900

    feat: バリデーション追加

--- nomanoma121/client/script.js
+++ nomanoma121/client/script.js
@@ -1,9 +1,33 @@
+const validateTodo = (todo) => {
+  if (typeof todo.id !== "number" || todo.id <= 0) {
+    throw new Error("Invalid ID");
+  }
+  if (typeof todo.title !== "string" || todo.title.length === 0) {
+    throw new Error("Invalid title");
+  }
+  if (typeof todo.completed !== "boolean") {
+    throw new Error("Invalid completed status");
+  }
+};
+
+const validateTodos = (todos) => {
+  if (!Array.isArray(todos)) {
+    throw new Error("Invalid todos format");
+  }
+  todos.forEach((todo) => {
+    validateTodo(todo);
+  });
+};
+
 const fetchTodos = async () => {
   const response = await fetch("http://localhost:8000/todo");
   if (!response.ok) {
     throw new Error("Network response was not ok");
   }
   const todos = await response.json();
+
+  validateTodos(todos);
+
   return todos;
 };
 
