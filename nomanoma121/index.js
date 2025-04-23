699a8a143d2a5f1358f686fd63f1d23134fa9d45
nomanoma121 <takeuchi180121@gmail.com>
2025-04-23 11:05:03 +0900

    feat: level-2実装

--- nomanoma121/index.js
+++ nomanoma121/index.js
@@ -1,1 +1,13 @@
-console.log("Hello, Node.js!");
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello, Honojs!", 200);
+});
+
+serve({
+  fetch: app.fetch,
+  port: 8000,
+});
