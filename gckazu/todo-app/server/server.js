e6e07b977a0b1a481acf99c19875ca79b4af5f79
gckazu <167419224+gckazu@users.noreply.github.com>
2025-04-24 19:47:31 +0900

    [Level 3] gckazu (#24)
    * todoアプリの基礎を作成
    
    * 空白を消去しました level-3 gckazu

---
+++ gckazu/todo-app/server/server.js
@@ -0,0 +1,21 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+
+const app = new Hono();
+app.use(cors({ origin: "*" }));
+
+const todoList = [
+    { id: 1, title: "JavaScriptを勉強する", completed: false }, 
+    { id: 2, title: "CTFのWriteupを書く", completed: false }, 
+    { id: 3, title: "学校の課題を提出する", completed: true }
+];
+// TODOリストの取得
+app.get("/todo", (c) => c.json(todoList, 200));
+
+
+serve({
+    fetch: app.fetch,
+    port: 8000,
+});
+
