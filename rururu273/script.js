747ed93285b59251fcef1c0125b5e705831e6bbc
rururu273 <167418690+rururu273@users.noreply.github.com>
2025-04-24 19:57:42 +0900

    Delete rururu273/script.js

--- rururu273/script.js
+++
@@ -1,9 +0,0 @@
-const fetchButton = document.getElementById("fetch");
-const output = document.getElementById("output");
-
-fetchButton.addEventListener("click", async () => {
-  const response = await fetch("http://localhost:8000");
-  console.log(response);
-  const data = await response.text();
-  output.innerHTML = data;
-});
