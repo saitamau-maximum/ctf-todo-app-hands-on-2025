70b61923b7b21c25711e68333fefbbcba82209fa
nomanoma121 <takeuchi.k.747@ms.saitama-u.ac.jp>
2025-04-24 16:04:15 +0900

    feat: 削除と編集を実装

--- nomanoma121/client/script.js
+++ nomanoma121/client/script.js
@@ -5,6 +5,7 @@ const apiRequest = async (path, method = "GET", body = null) => {
       "Content-Type": "application/json",
     },
   };
+  console.log("apiRequest", path, method, body);
   if (body) {
     options.body = JSON.stringify(body);
   }
@@ -20,14 +21,21 @@ const createTodoElement = (todo) => {
 
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
+  checkbox.dataset.id = todo.id;
   checkbox.checked = todo.completed;
 
   const textInput = document.createElement("input");
   textInput.type = "text";
+  textInput.dataset.id = todo.id;
   textInput.value = todo.title;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.dataset.id = todo.id;
 
   li.appendChild(checkbox);
   li.appendChild(textInput);
+  li.appendChild(deleteButton);
   return li;
 };
 
@@ -89,11 +97,13 @@ const renderTodos = async (todos) => {
 };
 
 (async () => {
-  const initialTodos = await apiRequest("/todo");
-  await renderTodos(initialTodos);
+  let todos = [];
+  todos = await apiRequest("/todo");
+  await renderTodos(todos);
 
   const todoForm = document.getElementById("todo-form");
   const todoInput = document.getElementById("todo-input");
+  const ul = document.getElementById("todo-list");
 
   todoForm.addEventListener("submit", async (e) => {
     e.preventDefault();
@@ -107,11 +117,73 @@ const renderTodos = async (todos) => {
       await apiRequest("/todo", "POST", newTodo);
       todoInput.value = "";
 
-      const todos = await apiRequest("/todo");
+      todos = await apiRequest("/todo");
       validateTodos(todos);
       await renderTodos(todos);
     } catch (error) {
       displayError(error);
+    }
+  });
+
+  // 要素ごとにやるやりかたがわからないため親要素にeventListenerを付与
+  // delete
+  ul.addEventListener("click", async (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const todoId = e.target.dataset.id;
+      try {
+        await apiRequest(`/todo/${todoId}`, "DELETE");
+        todos = await apiRequest("/todo");
+        validateTodos(todos);
+        await renderTodos(todos);
+      } catch (error) {
+        displayError(error);
+      }
+    }
+  });
+  
+  // カーソルを外すと編集を確定
+  ul.addEventListener(
+    "blur",
+    async (e) => {
+      if (e.target.tagName !== "INPUT" || e.target.type !== "text") {
+        return;
+      }
+      e.preventDefault();
+      const todoId = e.target.dataset.id;
+      const updatedTodo = {
+        title: e.target.value,
+        completed: todos.find((todo) => todo.id === Number.parseInt(todoId))
+          .completed,
+      };
+      try {
+        await apiRequest(`/todo/${todoId}`, "PUT", updatedTodo);
+        todos = await apiRequest("/todo");
+        console.log("todos", todos);
+        validateTodos(todos);
+        await renderTodos(todos);
+      } catch (error) {
+        displayError(error);
+      }
+    },
+    true // これをtrueにしないと親要素に伝番しないらしい
+  );
+
+  // change checkbox
+  ul.addEventListener("change", async (e) => {
+    if (e.target.tagName === "INPUT" && e.target.type === "checkbox") {
+      const todoId = e.target.dataset.id;
+      const updatedTodo = {
+        title: todos.find((todo) => todo.id === Number.parseInt(todoId)).title,
+        completed: e.target.checked,
+      };
+      try {
+        await apiRequest(`/todo/${todoId}`, "PUT", updatedTodo);
+        todos = await apiRequest("/todo");
+        validateTodos(todos);
+        await renderTodos(todos);
+      } catch (error) {
+        displayError(error);
+      }
     }
   });
 })();
